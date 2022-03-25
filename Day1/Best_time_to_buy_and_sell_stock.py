303cfa935b8480929624b222a242adc0d5830388
Aditya Trivedi <adityatrivedi08082001@gmail.com>
2022-03-25 18:47:14 +0530

    Add files via upload

---
+++ Day1/Best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxprofit=0
+        minsofar=prices[0]
+        for i in range(len(prices)):
+            minsofar=min(minsofar,prices[i])
+            maxprofit=max(maxprofit,prices[i]-minsofar)
+        return maxprofit
